0be6cf20a2f7c8d7f64a7abbff997be491559c52
liujj <liujj@kltong.com>
2019-09-04 14:31:06 +0800

    spring XmlBeanFactory 加载过程源码分析完成

--- src/main/java/com/ljj/ioc/chapter5/eventpublish/MethodExecutionEventPublisher.java
+++ src/main/java/com/ljj/ioc/chapter5/eventpublish/MethodExecutionEventPublisher.java
@@ -14,6 +14,8 @@ public class MethodExecutionEventPublisher {
 
     public void methodToMonitor() {
         MethodExectionEvent event2Publish = new MethodExectionEvent(this, "methodToMonitor");
+        publisherEvent(MethodExecutionStatus.BEGIN,event2Publish);
+        event2Publish.getMethodName();
         publisherEvent(MethodExecutionStatus.END, event2Publish);
     }
 
