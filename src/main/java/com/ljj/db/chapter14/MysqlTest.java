c0026bca470126e3a78613f95bcb79bd7200b306
LjjStuPid <1141547593@qq.com>
2019-09-15 21:45:44 +0800

    提交git忽略文件

--- src/main/java/com/ljj/db/chapter14/MysqlTest.java
+++ src/main/java/com/ljj/db/chapter14/MysqlTest.java
@@ -8,15 +8,19 @@ import java.util.Map;
 
 public class MysqlTest {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/quick4j?serverTimezone=Asia/Shanghai");
         basicDataSource.setUsername("root");
         basicDataSource.setPassword("123456");
-
         JdbcTemplate jdbcTemplate = new JdbcTemplate(basicDataSource);
-        basicDataSource.close();
-        Map map = jdbcTemplate.queryForMap("select * from user t where t.username = 'ljj'");
-        System.out.println(map);
+        try {
+            Map map = jdbcTemplate.queryForMap("select * from user t where t.username1 = 'ljj'");
+        }catch (Exception e){
+
+            System.out.println(((SQLException)e.getCause()).getErrorCode());
+        }
+
+//        System.out.println(map);
     }
 }
