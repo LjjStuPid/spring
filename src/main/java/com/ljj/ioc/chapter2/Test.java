b853a04c71c2c3b1db378d3187cfbc58edeb0526
liujj <liujj@kltong.com>
2019-09-25 18:46:34 +0800

    测试

--- src/main/java/com/ljj/ioc/chapter2/Test.java
+++ src/main/java/com/ljj/ioc/chapter2/Test.java
@@ -2,8 +2,6 @@ package com.ljj.ioc.chapter2;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.lang.reflect.Field;
-
 /**
  * @Author liujj
  * @Description TODO
@@ -11,21 +9,13 @@ import java.lang.reflect.Field;
  */
 public class Test {
 
-    private  final String m = "www";
+    private final String m = "www";
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-
-        Test test = new Test();
-        Field field = test.getClass().getDeclaredField("m");
-        field.setAccessible(true);
-        field.set(test,"fdafdasf");
-        System.out.println(field);
-
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.scan("com.ljj.ioc.chapter2");
         applicationContext.refresh();
         UserService userService = (UserService) applicationContext.getBean("userServiceImpl");
         System.out.println(userService);
-
     }
 }
