9297c16635171e6fdbfd8bdf0008fd9ab4adcfb2
liujj <liujj@kltong.com>
2019-09-26 17:04:42 +0800

    cglib

---
+++ src/main/java/com/ljj/ioc/chapter2/cglib/HelloService.java
@@ -0,0 +1,22 @@
+package com.ljj.ioc.chapter2.cglib;
+
+/**
+ * @Author liujj
+ * @Description TODO
+ * @Date 2019/9/26 11:08
+ */
+public final class HelloService {
+
+    public HelloService() {
+        System.out.println("HelloService构造");
+    }
+
+    static String sayOthers(String name) {
+        System.out.println("HelloService:sayOthers>>" + name);
+        return null;
+    }
+
+    void sayHello() {
+        System.out.println("HelloService:sayHello");
+    }
+}
